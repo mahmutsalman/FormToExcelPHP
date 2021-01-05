33b08eeedeba2824da87adc56da903999c5849ab
Enes <nasyrest@hotmail.com>
2021-01-05 23:40:49 +0300

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Excel</title>
+</head>
+<body>
+<form action= "urunDetay.php" method="post">
+ <p>Firma adı: <input type="text" name="firmaAdı" /></p>
+ <p>Yetkili adı: <input type="text" name="yetkiliAdi" /></p>
+ <p>Telefon: <input type="text" name="telefonNumarasi" /></p>
+ <p>E-posta: <input type="text" name="eposta" /></p>
+ <p>Fatura adresi: <input type="text" name="faturaAdresi" /></p>
+ <p>Vergi Dairesi/No <input type="text" name="vergiDaireNo" /></p>
+ <p><input type="submit" /></p>
+</form>
+
+
+
+    
+</body>
+</html>
---
+++ urunDetay.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adding Product Details</title>
+</head>
+<body>
+
+    <form method="post">
+    <p>ÜRÜN ADI <input type="text" name="name" /></p>
+    <p>MODEL <input type="text" name="age" /></p>
+    <p>ÖLÇÜ <input type="text" name="name" /></p>
+    <p>RENK <input type="text" name="age" /></p>
+    <p>MİKTAR <input type="text" name="name" /></p>
+    <p>BİRİM FİYATI <input type="text" name="age" /></p>
+    <p>TUTAR <input type="text" name="name" /></p>
+    <p>GÖRSEL <input type="text" name="age" /></p>
+   
+    <p><input type="submit" /></p>
+    </form>
+    
+</body>
+</html>
