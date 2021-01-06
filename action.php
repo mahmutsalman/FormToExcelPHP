ae3fdb77ee4cafd9b57e4343f7dbf79317848f6f
Enes <nasyrest@hotmail.com>
2021-01-06 16:03:43 +0300

    Taslak oluşturuldu

--- action.php
+++ action.php
@@ -8,9 +8,30 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello World !');
-$sheet->setCellValue('A2', 'Hello World-2 !');
-$sheet->setCellValue('A3', 'Hello World-3 !');
+$sheet->setCellValue('A3','FİYAT TEKLİFİ');
+$sheet->setCellValue('A4', 'FİRMA ADI !');
+$sheet->setCellValue('A5', 'YETKİLİ ADI');
+$sheet->setCellValue('A6', 'TELEFON');
+$sheet->setCellValue('A7','E-POSTA');
+$sheet->setCellValue('A8','FATURA ADRESİ');
+$sheet->setCellValue('A9','VERGİ DAİRESİ/NO');
+$sheet->setCellValue('A10','S.NO');
+$sheet->setCellValue('B10','ÜRÜN ADI ');
+$sheet->setCellValue('C10','MODEL ');
+$sheet->setCellValue('D10','ÖLÇÜ ');
+$sheet->setCellValue('E10','RENK');
+$sheet->setCellValue('F10','MİKTAR');
+$sheet->setCellValue('G10','BİRİM FİYATI ');
+$sheet->setCellValue('H10','TUTAR');
+$sheet->setCellValue('I10','GÖRSEL ');
+
+
+
+
+
+
+
+
 
 
 
